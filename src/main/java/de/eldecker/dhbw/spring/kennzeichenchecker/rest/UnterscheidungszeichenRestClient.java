e3c4e5f0e4a0459c0093507a5d076f1b7d279864
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-21 19:08:12 +0100

    logdatei.log geloescht (versehentlich committed, bevor entsprechender Eintrag in gitignore da war)

--- src/main/java/de/eldecker/dhbw/spring/kennzeichenchecker/rest/UnterscheidungszeichenRestClient.java
+++ src/main/java/de/eldecker/dhbw/spring/kennzeichenchecker/rest/UnterscheidungszeichenRestClient.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
