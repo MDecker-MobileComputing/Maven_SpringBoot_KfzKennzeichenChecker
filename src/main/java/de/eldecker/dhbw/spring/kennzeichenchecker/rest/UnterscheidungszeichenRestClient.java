6389ec7c256740adcd2d17c7664bfffc2061f423
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-03 11:06:01 +0100

    UnterscheidungszeichenRestClient: Whitespaces verbessert

--- src/main/java/de/eldecker/dhbw/spring/kennzeichenchecker/rest/UnterscheidungszeichenRestClient.java
+++ src/main/java/de/eldecker/dhbw/spring/kennzeichenchecker/rest/UnterscheidungszeichenRestClient.java
@@ -26,27 +26,26 @@ import de.eldecker.dhbw.spring.kennzeichenchecker.model.extern.Unterscheidungsze
  * Diese Bean implementiert den REST-Client, mit dem von einer externen
  * REST-API die Gültigkeit eines Unterscheidungszeichen (ein bis drei
  * Buchstaben ganz am Anfang eines deutschen KFZ-Kennzeichens) abgefragt
- * werden kann. 
+ * werden kann.
  * <br><br>
- * 
+ *
  * Microservice/REST-API für Abfrage Unterscheidungszeichen:
  * <a href="https://github.com/MDecker-MobileComputing/Maven_SpringBoot_KfzUnterscheidungszeichen">siehe hier</a>
  */
 @Service
 @EnableCaching
-@EnableScheduling 
+@EnableScheduling
 @CacheConfig( cacheNames = "unterscheidungszeichenCache" )
 public class UnterscheidungszeichenRestClient {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger( UnterscheidungszeichenRestClient.class );
-    
+
     /** Leeres Unterscheidungszeichenobjekt, wird im Fehlerfall benötigt. */
     private static final Unterscheidungszeichen UNTERSCHEIDUNGSZEICHEN_EMPTY =
                                                         new Unterscheidungszeichen( "", "", NICHT_DEFINIERT );
-
     /** Objekt für REST-Calls. */
     private final RestClient _restClient;
-    
+
 
     /**
      * Konstruktor für Erzeugung {@code RestClient}-Objekt mit
@@ -58,75 +57,75 @@ public class UnterscheidungszeichenRestClient {
         _restClient = restClientBuilder.baseUrl( "http://localhost:8080/unterscheidungszeichen" )
                                        .build();
     }
-    
-    
+
+
     /**
      * Unterscheidungszeichen (z.B. "KA" für "Karlsruhe") von anderer REST-API abfragen.
-     *  
-     * @param unterscheidungszeichen Unterscheidungszeichen, das überprüft werden soll; 
+     *
+     * @param unterscheidungszeichen Unterscheidungszeichen, das überprüft werden soll;
      *                               sollte schon normalisiert sein.
-     *                               
+     *
      * @return Antwort von externem REST-Service, wird nicht {@code null} sein
      */
     @Cacheable( value = "unterscheidungszeichenCache" )
     public RestErgebnisRecord holeUnterscheidungszeichen( String unterscheidungszeichen ) {
-        
+
         ResponseEntity<RestErgebnisRecord> ergebnisEntity = null;
-        
+
         final String pfad = "/v1/suche/" + unterscheidungszeichen;
         LOG.info( "Sende jetzt HTTP-Request an folgenden Pfad: {}", pfad );
-                
+
         try {
- 
+
             ergebnisEntity = _restClient.get()
                                         .uri( pfad )
                                         .retrieve()
-                                        .toEntity( RestErgebnisRecord.class );        
-                                                
+                                        .toEntity( RestErgebnisRecord.class );
+
             final Unterscheidungszeichen uz = ergebnisEntity.getBody().unterscheidungszeichen();
-            
-            return new RestErgebnisRecord( true, "", uz ); 
-        }        
+
+            return new RestErgebnisRecord( true, "", uz );
+        }
         catch ( RestClientResponseException ex ) {
-            
+
             final int httpStatusCode = ex.getStatusCode().value();
             switch ( httpStatusCode ) {
 
                 case 400:
                     LOG.error( "Unterscheidungszeichen hat unzulässiges Format." );
-                    return new RestErgebnisRecord( 
-                                        false, 
-                                        "Unterscheidungszeichen \"" + unterscheidungszeichen + "\" hat unzulässiges Format", 
+                    return new RestErgebnisRecord(
+                                        false,
+                                        "Unterscheidungszeichen \"" + unterscheidungszeichen + "\" hat unzulässiges Format",
                                         UNTERSCHEIDUNGSZEICHEN_EMPTY );
                 case 404:
                     LOG.warn( "Unterscheidungszeichen existiert nicht: " + unterscheidungszeichen );
-                    return new RestErgebnisRecord( 
-                                        false, 
-                                        "Unterscheidungszeichen \"" + unterscheidungszeichen + "\" wurde nicht gefunden", 
-                                        UNTERSCHEIDUNGSZEICHEN_EMPTY );                    
+                    return new RestErgebnisRecord(
+                                        false,
+                                        "Unterscheidungszeichen \"" + unterscheidungszeichen + "\" wurde nicht gefunden",
+                                        UNTERSCHEIDUNGSZEICHEN_EMPTY );
                 default:
-                    LOG.error( "Externer Service hat unerwarteten Fehler-Code zurückgeliefert: " + httpStatusCode ); 
-                    return new RestErgebnisRecord( 
-                                        false, 
-                                        "Unerwarteter HTTP-Status-Code " + httpStatusCode + " von Service erhalten.", 
-                                        UNTERSCHEIDUNGSZEICHEN_EMPTY );                    
+                    LOG.error( "Externer Service hat unerwarteten Fehler-Code zurückgeliefert: " + httpStatusCode );
+                    return new RestErgebnisRecord(
+                                        false,
+                                        "Unerwarteter HTTP-Status-Code " + httpStatusCode + " von Service erhalten.",
+                                        UNTERSCHEIDUNGSZEICHEN_EMPTY );
             }
-        }        
+        }
         catch ( ResourceAccessException ex ) {
 
             LOG.error( "Externer Service für Unterscheidungszeichen-Abfrage war nicht erreichbar." );
-            
-            return new RestErgebnisRecord( 
-                    false, 
-                    "Service für Unterscheidungszeichen war nicht erreichbar",  
-                    UNTERSCHEIDUNGSZEICHEN_EMPTY );            
+
+            return new RestErgebnisRecord(
+                    false,
+                    "Service für Unterscheidungszeichen war nicht erreichbar",
+                    UNTERSCHEIDUNGSZEICHEN_EMPTY );
         }
     }
-    
-    
+
+
     /**
-     * Diese Methode ist mit {@code Scheduled} annotiert, so dass sie perodisch aufgerufen wird. 
-     * Bei jedem Aufruf wird der Cache gelöscht. Die Zeitwerte für die Annotation {@code Scheduled} 
+     * Diese Methode ist mit {@code Scheduled} annotiert, so dass sie perodisch aufgerufen wird.
+     * Bei jedem Aufruf wird der Cache gelöscht. Die Zeitwerte für die Annotation {@code Scheduled}
      * sind in Millisekunden.
      */
     @Scheduled( fixedRate = 120_000, initialDelay = 120_000 )
@@ -134,6 +133,6 @@ public class UnterscheidungszeichenRestClient {
     public void cacheLoeschen() {
 
         LOG.info( "Der Unterscheidungszeichen-Cache wurde geleert." );
-    }    
-    
+    }
+
 }
